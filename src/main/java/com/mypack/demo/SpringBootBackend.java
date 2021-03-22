e2def1279287d5020f7a4f510f78a77bbcedc5d5
Janitha Missaka <janitha.missaka@gmail.com>
2021-03-22 15:55:59 +0530

    Initial commit

---
+++ src/main/java/com/mypack/demo/SpringBootBackend.java
@@ -0,0 +1,13 @@
+package com.mypack.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBackend {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBackend.class, args);
+    }
+
+}
